7269a3e5b8f88849eae02952a533d3add015ce62
AndyGeek <andy.eulogio@gmail.com>
2020-06-11 00:50:02 -0500

    update

---
+++ app.js
@@ -0,0 +1,20 @@
+const miapp = new Vue({
+    el: '#app',
+    data: {
+        productos: [
+            {nombre: 'Colores largos', precio: 12.0, cantidad: 5},
+            {nombre: 'Cuaderno cuadriculado', precio: 3.50, cantidad: 2},
+        ],
+        nuevoNombre: '',
+        nuevoPrecio: 0,
+        nuevoCantidad: 0
+    },
+    methods: {
+        agregar: function(){
+            this.productos.push({nombre: this.nuevoNombre, precio: this.nuevoPrecio, cantidad: this.nuevoCantidad});
+            this.nuevoNombre = '',
+            this.nuevoPrecio = '',
+            this.nuevoCantidad = ''            
+        }
+    }
+});
